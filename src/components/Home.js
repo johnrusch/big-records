ba8ddd2f2ffb8616a5946c59d6487d0256c6e512
johnrusch <johnrusch7@gmail.com>
2022-04-14 00:55:11 -0700

    adds functioning masons room :)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import Carousel from "./Carousel";
 import defaultSlides from "./defaultSlides";
 import SocialLinks from "./SocialLinks";
@@ -6,36 +7,7 @@ import useWindowDimensions from "../hooks/useWindowDimensions";
 import { ReactComponent as InstagramIcon } from "../assets/svgs/instagram-icon.svg";
 
 const Home = () => {
-  const strings = [
-    "Welcome to the Big Room",
-    "The Big Room is a place to relax and enjoy the outdoors",
-    "Dobbs Room",
-    "Control is key",
-    "Feet are for walking",
-    "Turf is for playing",
-    "Smells are for smelling",
-    "Hello friends",
-    "I am a big room",
-    "It's a big room",
-    "Let's go big",
-    "Nice to meet you",
-    "Incredible",
-    "Sweet",
-    "You are a big room",
-    "We are a big room",
-    "C'mon...",
-    "Please take a seat",
-    "Enjoy your stay",
-    "How are you",
-    "Haha, yeah...",
-  ];
-
   const { height, width } = useWindowDimensions();
-
-  const getRandomString = () => {
-    const randomIndex = Math.floor(Math.random() * strings.length);
-    return strings[randomIndex];
-  };
 
   // gets image from the image api
   const getImage = async () => {
@@ -45,18 +17,23 @@ const Home = () => {
     return data;
   };
 
-  useEffect(() => {
-    setInterval(() => {
-      document.title = getRandomString();
-    }, 3000);
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <div className="homeScreen">
       <SocialLinks />
       <div className="homeBackground1">
-        <img style={{ width }} src="https://big-records.s3.us-west-2.amazonaws.com/assets/ezgif.com-gif-maker.gif" alt="big records background" />
+        <img
+          style={{ width }}
+          src="https://big-records.s3.us-west-2.amazonaws.com/assets/ezgif.com-gif-maker.gif"
+          alt="big records background"
+        />
       </div>
+      <Link to="/masons-room">
+        <div className="masonsRoomButton">
+          <h1>Masons Room</h1>
+        </div>
+      </Link>
       <div style={{ height }} className="homeBackground2">
         <video autoPlay muted loop className="homeBackgroundVideo">
           <source
--- src/components/MasonsRoom.js
+++ src/components/MasonsRoom.js
@@ -1,14 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import useWindowDimensions from "../hooks/useWindowDimensions";
+import { ReactComponent as Volume } from "../assets/svgs/volume.svg";
+import { ReactComponent as VolumeMute } from "../assets/svgs/volume-mute.svg";
 
 const MasonsRoom = () => {
+    const [audioMuted, setAudioMuted] = useState(true);
 
-    return (
-        <div className="masonsRoom">
-            <audio controls loop>
-                <source src="https://d29f99edrn2ccp.cloudfront.net/green,%20yellow,%20orange,%20red,%20brown%2012_28.m4a" type="audio/mpeg" />
-            </audio>
-        </div>
-    )
+    const { height, width } = useWindowDimensions();
+    
+    const renderVolumeIcon = () => {
+        if (audioMuted) {
+            return <VolumeMute className="volumeMuted" fill="black" />;
+        } else {
+            return <Volume className="volume" fill="black" />;
+        }
+    };
+
+    const toggleAudioMuted = () => {
+        const audioElement = document.getElementById("audioElement");
+        if (audioElement.paused) audioElement.play();
+        setAudioMuted(!audioMuted);
+    };
+
+
+    useEffect(() => {
+    
+    }, []);
+
+  return (
+    <div className="masonsRoom">
+      <audio id="audioElement" loop autoPlay muted={audioMuted}>
+        <source
+          src="http://35.165.227.26:8000/masons_room"
+          type="audio/mpeg"
+        />
+      </audio>
+      {/* <iframe src="http://35.165.227.26:8000/masons_room" allow="autoplay" style={{ display: 'none' }} title="mason's mix"/> */}
+      <div style={{ height }} className="masonsRoomBackground">
+        <video autoPlay loop muted>
+          <source
+            src="https://big-records.s3.us-west-2.amazonaws.com/assets/Calmmovie.mp4"
+            type="video/mp4"
+          />
+        </video>
+      </div>
+      <div className="volumeIcon" onClick={toggleAudioMuted}>
+          {renderVolumeIcon()}
+      </div>
+    </div>
+  );
 };
 
 export default MasonsRoom;
